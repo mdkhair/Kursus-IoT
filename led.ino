6931418a693e00d9da26e2756a37b5f0498c5d26
Ibnu Noordin <129460438+mdkhair@users.noreply.github.com>
2024-05-29 21:24:47 +0800

    Create led.ino

---
+++ led.ino
@@ -0,0 +1,32 @@
+#define BLYNK_PRINT Serial
+#define BLYNK_TEMPLATE_ID "XXXXXXXXXX"
+#define BLYNK_TEMPLATE_NAME "XXXXXXXX"
+#define BLYNK_AUTH_TOKEN "XXXXXXXXXXXXX"
+
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+// WiFi credentials
+char ssid[] = "Wokwi-GUEST";
+char pass[] = "";
+
+// This function will be called every time device receives command from Blynk App
+BLYNK_WRITE(V0) {
+  int pinValue = param.asInt(); 
+  digitalWrite(LED_PIN, pinValue); 
+}
+
+}
+void setup()
+{
+  Serial.begin(9600);
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+  pinMode(22, OUTPUT); 
+}
+
+void loop()
+{
+  Blynk.run();
+}
